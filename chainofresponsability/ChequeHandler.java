b2181803ef54467abd8262ec4d6932f44c1a194d
Giovanni Sencioles <66632486+Sensacioles@users.noreply.github.com>
2022-10-31 08:47:15 -0300

    add chainofresponsability

---
+++ chainofresponsability/ChequeHandler.java
@@ -0,0 +1,20 @@
+package chainofresponsability;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class ChequeHandler  extends AbstractPagamentoHandler {
+    public static final int CHEQUE = 3;
+    @Override
+    protected int  handlePagamento(int valor) {
+        int valorRestante = super.perguntaPagamento("Qual o valor para o "
+                + "pagamento com Cheque?", valor);
+        JOptionPane.showMessageDialog(null, "Cheque gerado. Valor restante:" 
+                + valorRestante);
+        return valorRestante;
+    }
+    @Override
+    protected int getTipoPagamento() {
+        return CHEQUE;
+    }
+}
